f0dc537c1f054f08f5eb9d8f002d4d3e76af380b
Taylorbelk1 <belktaylor12@gmail.com>
2020-08-09 13:57:05 -0500

    added update and delete for tickets and notes

--- api/tickets/noteRoutes.js
+++ api/tickets/noteRoutes.js
@@ -6,7 +6,9 @@ const notesHelper = require("../../database/helpers/ticketNotesHelper");
 const errors = {
     cantGetNotes: "Unable to retrieve the notes for this ticket",
     noTicketsForNote: "There are no notes for this ticket",
-    cantCreateNote: "Unable to create the note"
+    cantCreateNote: "Unable to create the note",
+    cantUpdate: "Unable to update the note",
+    cantDelete: "Unable to delete the note"
 }
 
 router.post("/getNotesForTicket", async(req, res) => {
@@ -27,6 +29,32 @@ router.post("/addNewNote", async(req, res) => {
         console.log(n);
         if(!n) res.status(400).json({ message: errors.cantCreateNote })
         else res.status(200).json(n)
+    }).catch(err => {
+        res.status(500).json({ err, message: errors.cantCreateNote })
+    })
+});
+
+router.put("/updateNote", async(req, res) => {
+    let note = req.body;
+
+    notesHelper.updateNote(note).then(n => {
+        console.log(n);
+
+        if(!n) res.status(400).json({ message: errors.cantUpdate })
+        else res.status(200).json(n)
+    }).catch(err => {
+        res.status(500).json({ err, message: errors.cantCreateNote })
+    })
+});
+
+router.delete("/deleteNote", async(req, res) => {
+    let id = req.body;
+
+    notesHelper.deleteNote(id).then(i => {
+        console.log(i);
+
+        if(!i) res.status(400).json({ message: errors.cantDelete })
+        else res.status(200).json(i)
     }).catch(err => {
         res.status(500).json({ err, message: errors.cantCreateNote })
     })
--- api/tickets/ticketRoutes.js
+++ api/tickets/ticketRoutes.js
@@ -6,7 +6,9 @@ const ticketHelper = require("../../database/helpers/ticketHelper");
 
 const errors = {
     cantGetTickets: "Unable to retrieve user's assigned tickets",
-    cantCreate: "Unable to create ticket"
+    cantCreate: "Unable to create ticket",
+    cantUpdate: "Unable to update the ticket",
+    cantDelete: "Unable to delete the ticket"
 }
 
 router.post("/getUserTickets", async(req, res) => {
@@ -31,6 +33,30 @@ router.post("/createNewTicket", async(req, res) => {
         else res.status(400).json({ message: errors.cantCreate })
     }).catch(err => {
         res.status(500).json({ err, message: errors.cantCreate })
+    })
+})
+
+router.put("/updateTicket", async(req, res) => {
+    console.log("in create new ticket");
+    let ticket = req.body;
+    console.log(req.body)
+
+    ticketHelper.updateTicket(ticket).then(id => {
+        if(id.length > 0) res.status(200).json({ id })
+        else res.status(400).json({ message: errors.cantUpdate })
+    }).catch(err => {
+        res.status(500).json({ err, message: errors.cantUpdate })
+    })
+});
+
+router.delete("/deleteTicket", async(req, res) => {
+    let id = req.body;
+
+    ticketHelper.deleteTicket(id).then(i => {
+        if(id.length > 0) res.status(200).json({ id })
+        else res.status(400).json({ message: errors.cantDelete })
+    }).catch(err => {
+        res.status(500).json({ err, message: errors.cantDelete })
     })
 })
 
