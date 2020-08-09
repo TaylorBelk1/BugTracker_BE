3c4e01456686bbea541b4f4dc7255eec658f1200
Taylorbelk1 <belktaylor12@gmail.com>
2020-08-09 15:23:43 -0500

    change varchar to text for pg

--- database/migrations/20200623154211_adminUsers.js
+++ database/migrations/20200623154211_adminUsers.js
@@ -2,11 +2,11 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("admin_users", user => {
         user.increments();
-        user.varchar("username", 24).notNullable().unique();
-        user.varchar("password", 24).notNullable();
+        user.text("username", 24).notNullable().unique();
+        user.text("password", 24).notNullable();
         user.text("first_name", 255).notNullable();
         user.text("last_name", 255).notNullable();
-        user.varchar("email", 255).notNullable().unique();
+        user.text("email", 255).notNullable().unique();
       });
 };
 
