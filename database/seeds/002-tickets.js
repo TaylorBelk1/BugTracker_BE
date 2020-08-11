0fb6a8d58deac6a6fc65c670ad1fbc01a6efdc7b
Taylorbelk1 <belktaylor12@gmail.com>
2020-08-11 12:53:44 -0500

    fixing seed

--- database/seeds/002-tickets.js
+++ database/seeds/002-tickets.js
@@ -1,26 +1,26 @@
 
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return knex('tickets').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('tickets').insert([
-//         {
-//           id: 1, 
-//           name: 'Test ticket',
-//           description: "This is a test ticket. Inserting random information now thanks",
-//           created_by: "testuser",
-//           assigned_to: "tbelk1234",
-//           created_at: Date.now().toString()
-//         },
-//         {
-//           id: 2, 
-//           name: 'Test ticket 2',
-//           description: "This is another test ticket. Inserting random information now thanks",
-//           created_by: "testuser",
-//           assigned_to: "tbelk1234",
-//           created_at: Date.now().toString()
-//         },
-//       ]);
-//     });
-// };
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('tickets').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('tickets').insert([
+        // {
+        //   id: 1, 
+        //   name: 'Test ticket',
+        //   description: "This is a test ticket. Inserting random information now thanks",
+        //   created_by: "testuser",
+        //   assigned_to: "tbelk1234",
+        //   created_at: Date.now().toString()
+        // },
+        // {
+        //   id: 2, 
+        //   name: 'Test ticket 2',
+        //   description: "This is another test ticket. Inserting random information now thanks",
+        //   created_by: "testuser",
+        //   assigned_to: "tbelk1234",
+        //   created_at: Date.now().toString()
+        // },
+      ]);
+    });
+};
--- database/seeds/003-ticket-notes.js
+++ database/seeds/003-ticket-notes.js
@@ -1,24 +1,24 @@
 
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return knex('ticket_notes').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('ticket_notes').insert([
-//         {
-//           id: 1, 
-//           created_by: "tbelk1234",
-//           created_at: Date.now().toString(),
-//           ticket_id: 1,
-//           note: "This is a random note for a test ticket. There is an issue somewhere that we need to fix!"
-//         },
-//         {
-//           id: 2, 
-//           created_by: "tbelk1234",
-//           created_at: Date.now().toString(),
-//           ticket_id: 1,
-//           note: "This is another random note for a test ticket. There is an issue somewhere that we need to fix!"
-//         }
-//       ]);
-//     });
-// };
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('ticket_notes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('ticket_notes').insert([
+        // {
+        //   id: 1, 
+        //   created_by: "tbelk1234",
+        //   created_at: Date.now().toString(),
+        //   ticket_id: 1,
+        //   note: "This is a random note for a test ticket. There is an issue somewhere that we need to fix!"
+        // },
+        // {
+        //   id: 2, 
+        //   created_by: "tbelk1234",
+        //   created_at: Date.now().toString(),
+        //   ticket_id: 1,
+        //   note: "This is another random note for a test ticket. There is an issue somewhere that we need to fix!"
+        // }
+      ]);
+    });
+};
