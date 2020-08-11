36297621c9078feadf9d648bb90a0300fa3d54d2
Taylorbelk1 <belktaylor12@gmail.com>
2020-08-11 11:58:10 -0500

    change to query to try and solve postgres issue

--- database/helpers/ticketNotesHelper.js
+++ database/helpers/ticketNotesHelper.js
@@ -9,7 +9,7 @@ module.exports = {
 
 async function getNotesForTicketId(ticket_id) {
     console.log(ticket_id)
-    let notes = await db("ticket_notes").where({ticket_id});
+    let notes = await db("ticket_notes").where('ticket_id', ticket_id);
     console.log(notes);
     return notes
 }
